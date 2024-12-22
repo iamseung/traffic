581222ed89345451eba16e7bae6740d81c3f979d
iamseung <seesungseok@naver.com>
2024-12-22 16:50:48 +0900

    [20241222] 탈퇴 기능
    - 탈퇴 기능 가안 업데이트

---
+++ src/main/java/com/example/traffic/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.traffic.controller;
+
+import com.example.traffic.entity.User;
+import com.example.traffic.service.UserService;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestParam String username, @RequestParam String password, @RequestParam String email) {
+        User user = userService.createUser(username, password, email);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(
+            @Parameter(description = "ID of the user to be deleted", required = true) @PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build();
+    }
+}
