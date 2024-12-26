e8cda372acfe27084abe1a70dd3f06eed68a2b6c
iamseung <seesungseok@naver.com>
2024-12-26 23:19:29 +0900

    [20241226] 블랙리스트 기능 구현
    - 블랙리스트 기능 구현 위한 초기 작업
    - 메서드 구현

--- src/main/java/com/example/traffic/jwt/JwtUtil.java
+++ src/main/java/com/example/traffic/jwt/JwtUtil.java
@@ -47,4 +47,12 @@ public class JwtUtil {
                 .getBody();
         return claims.getSubject();
     }
+
+    public Date getExpirationDateFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getExpiration();
+    }
 }
