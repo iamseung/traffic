92369fa21e9d120dabb37d68c9710d858e48e44f
iamseung <seesungseok@naver.com>
2025-04-08 22:26:53 +0900

    ✨ Batch 설정 추가

---
+++ src/main/java/pojo/SendCommentNotification.java
@@ -0,0 +1,14 @@
+package pojo;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class SendCommentNotification {
+    private String type = "send_comment_notification";
+    private Long commentId;
+    private Long userId;
+}
